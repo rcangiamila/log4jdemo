12d64dd6efa748d7636253f66770cbca2f5a98cf
Roberto Cangiamila <roberto.cangiamila@par-tec.it>
2018-07-26 14:53:54 +0200

    Add log4j2 configuration file

--- src/main/java/it/partec/log4jdemo/Log4jdemoApplication.java
+++ src/main/java/it/partec/log4jdemo/Log4jdemoApplication.java
@@ -1,13 +1,26 @@
 package it.partec.log4jdemo;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
-public class Log4jdemoApplication {
+public class Log4jdemoApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
 
         SpringApplication.run(Log4jdemoApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        log.debug("Debugging log");
+        log.info("Info log");
+        log.warn("Hey, This is a warning!");
+        log.error("Oops! We have an Error. OK");
+        log.fatal("Damn! Fatal error. Please fix me.");
     }
 }
