f1c23f70f96bcaeedaef3f90edcea86ca2f9df95
Roberto Cangiamila <roberto.cangiamila@par-tec.it>
2018-07-26 18:18:00 +0200

    Fix log writing transformation;

--- src/main/java/it/partec/log4jdemo/Log4jdemoApplication.java
+++ src/main/java/it/partec/log4jdemo/Log4jdemoApplication.java
@@ -3,7 +3,9 @@ package it.partec.log4jdemo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thedeanda.lorem.Lorem;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,6 +26,9 @@ public class Log4jdemoApplication implements ApplicationRunner {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Value("${log.max.characters.string}")
+    private Integer logMaxCharactersString;
+
     public static void main(String[] args) {
 
         SpringApplication.run(Log4jdemoApplication.class, args);
@@ -41,9 +46,11 @@ public class Log4jdemoApplication implements ApplicationRunner {
 
             Map<String, Object> fieldsMap = new HashMap<>();
 
-            for (int i = 0;i<10;i++) {
-                fieldsMap.put(String.format("key_%d", i), loremIpsum.getParagraphs(2, 4));
-            }
+            StringBuffer sb = new StringBuffer(4096);
+
+            sb.append(RandomStringUtils.random(logMaxCharactersString, true, true));
+
+            fieldsMap.put("key", sb.toString());
 
             log.info(objectMapper.writeValueAsString(fieldsMap));
 
