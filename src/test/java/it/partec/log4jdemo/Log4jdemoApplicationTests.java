c179c312bb8e4aded026c51bbfa85928f463c56b
Roberto Cangiamila <roberto.cangiamila@par-tec.it>
2018-07-26 14:38:11 +0200

    Initial Import ...

---
+++ src/test/java/it/partec/log4jdemo/Log4jdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package it.partec.log4jdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Log4jdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
